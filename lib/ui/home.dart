abe51ee45023cd71111d7aac3ea4738c94a85b81
mahdiyarz <m.arbabzi@gmail.com>
2022-03-10 23:30:41 +0330

    Create a button and qoute's list on screen

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -10,10 +10,23 @@ class Wisdom extends StatefulWidget {
 class _WisdomState extends State<Wisdom> {
   @override
   Widget build(BuildContext context) {
+    List<String> qoutes = [
+      "Change your thoughts and you change your world.",
+      "Imagination is everything. It is the preview of life’s coming attractions.",
+      "Happiness is not something ready made. It comes from your own actions."
+    ];
     return Scaffold(
         body: Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [Text('data')],
+      children: [
+        Text(qoutes[0]),
+        TextButton.icon(
+            onPressed: _showNextQuote,
+            icon: Icon(Icons.skip_next),
+            label: Text('Next Quote'))
+      ],
     ));
   }
+
+  void _showNextQuote() {}
 }
