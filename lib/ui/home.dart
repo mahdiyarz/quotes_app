f4ebfd41a70d364dcf5d2aeb18acb6436e61b244
mahdiyarz <m.arbabzi@gmail.com>
2022-03-11 00:25:49 +0330

    Work on style

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -18,16 +18,38 @@ class _WisdomState extends State<Wisdom> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(qoutes[index % qoutes.length]),
-        TextButton.icon(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 350,
+            height: 200,
+            margin: const EdgeInsets.all(30),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: Colors.amber),
+            child: Center(
+              child: Text(
+                qoutes[index % qoutes.length],
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ),
+          const Divider(),
+          ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(primary: Colors.green),
             onPressed: _showNextQuote,
-            icon: Icon(Icons.skip_next),
-            label: Text('Next Quote'))
-      ],
-    ));
+            icon: const Icon(Icons.skip_next),
+            label: const Text('Next Quote'),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
   }
 
   void _showNextQuote() {
